413add976fd5d821b0cfea3f0afcfe5bbfc0da49
sankar269 <sankarsd596@gmail.com>
2024-06-18 12:12:23 +0530

    first commit

---
+++ calculator.py
@@ -0,0 +1,38 @@
+def add(x, y):
+    return x + y
+
+
+def subtract(x, y):
+    return x - y
+
+
+def multiply(x, y):
+    return x * y
+
+
+def divide(x, y):
+    if y == 0:
+        return "Error: Division by zero"
+    return x / y
+
+
+print("Calculator")
+x = int(input("Enter the first number"))
+y = int(input("Enter the second number"))
+choice = int(input("""1.Add/n
+         2.Subtract/n
+         3.multiply/n
+         4.divide/n
+         Enter the choice :-"""))
+
+if choice == 1:
+    print("The addition is",add(x,y))
+elif choice == 2:
+    print("The difference is:",subtract(x,y))
+elif choice == 3:
+    print("The product is :",multiply(x,y))
+elif choice == 4:
+    print("The division is:",divide(x,y))
+else:
+    print("Invalid choice try again")
+
